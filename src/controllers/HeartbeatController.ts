df294776ff96392f04128c63050b3922c92eef12
Sadat Islam <sadat21@hotmail.ca>
2020-04-04 15:50:08 -0600

    Don't store duplicate members on heartbeat

--- src/controllers/HeartbeatController.ts
+++ src/controllers/HeartbeatController.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
-import { firebaseObject } from "../config/Firebase";
-import moment from "moment";
-import { DB_COLLECTION_CHATROOMS } from "../config/constants";
+import { Request, Response } from 'express';
+import { firebaseObject } from '../config/Firebase';
+import moment from 'moment';
+import { DB_COLLECTION_CHATROOMS } from '../config/constants';
 
 export class HeartbeatController {
   private static validateBody(body: any) {
@@ -9,36 +9,39 @@ export class HeartbeatController {
   }
 
   public handleHeartBeat(req: Request, res: Response) {
-    console.log("Heartbeat received");
+    console.log('Heartbeat received');
 
     const body = req.body;
     if (!HeartbeatController.validateBody(body)) {
-      console.warn("Incorrect heartbeat received");
-      res.send("Incorrect heartbeat received");
+      console.warn('Incorrect heartbeat received');
+      res.send('Incorrect heartbeat received');
       return;
     }
 
     firebaseObject.DB.collection(DB_COLLECTION_CHATROOMS)
       .doc(body.chatId)
       .get()
-      .then(doc => {
+      .then((doc) => {
         if (doc) {
-          const members = doc.data()?.members as any[];
+          const members = (doc.data()?.members as any[]).filter((value) => {
+            return value.userId !== body.userId;
+          });
+
           members.push({
             userId: body.userId,
             ip: body.ip,
             port: body.port,
-            lastSeen: moment().valueOf()
+            lastSeen: moment().valueOf(),
           });
 
           firebaseObject.DB.collection(DB_COLLECTION_CHATROOMS)
             .doc(body.chatId)
             .set({ members: members }, { merge: true })
             .then(() => {
-              res.send("Received Heartbeat");
+              res.send('Received Heartbeat');
             });
         } else {
-          res.send("Chatroom id does not exist");
+          res.send('Chatroom id does not exist');
         }
       });
   }
